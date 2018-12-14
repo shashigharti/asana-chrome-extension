45172ab0838efb7708a6cb12da47b982a50d8928
shashi.gharti@gmail.com <shashi.gharti@gmail.com>
2018-12-14 15:36:01 +0545

    initial commit

---
+++ js/content.js
@@ -0,0 +1,7 @@
+$(function(){
+    let tasks = $('.TaskList').find('div.TaskRow');
+    $.each(tasks, function(index, task){
+        $(task).find('.ItemRowTwoColumnStructure-right').append('<div class="Button Button--small PotGridHeader-button PotGridHeader-availabilityTaskButton" tabindex="0" role="button" aria-disabled="false" aria-pressed="false">Availability</div>');
+        $(task).find('.ItemRowTwoColumnStructure-right').append('<div class="Button Button--small PotGridHeader-button PotGridHeader-unassignTaskButton" tabindex="0" role="button" aria-disabled="false" aria-pressed="false">UnAssign</div>');
+    });
+});
